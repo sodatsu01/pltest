33bc5d76fbf10370439e2a86371527b2d4329a5b
sodatsu01 <darumasanga.korondaa@gmail.com>
2022-03-25 10:57:22 +0900

    detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("io.gitlab.arturbosch.detekt") version "1.19.0"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
+
+apply(plugin = "io.gitlab.arturbosch.detekt")
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
